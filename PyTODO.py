be8857ba9d8608f6b771b05bb8148f261f794dbe
Kvkr3 <kulevgen32@gmail.com>
2019-04-06 15:38:27 +0600

    Fixed prints and command-checking system

--- PyTODO.py
+++ PyTODO.py
@@ -5,16 +5,16 @@ todo_list = loads(todo_list)
 
 
 def view_todo_list():
-    print("Список задач: \n")
     for i, c in enumerate(todo_list):
         print('[', i, ']', todo_list[i]['task'], '|', todo_list[i]['status'])
 
 
 while True:
+    print("Список задач: \n")
     view_todo_list()
     print('\nВведите номер задачи или введите "+" чтобы добавить новую')
     command = input("> ")
-    if command != "+":
+    if command == "+":
         pass
     else:
         pass
