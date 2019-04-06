b4aa0b74ab82230efa10ebdf3bb062862d5b333b
Kvkr3 <kulevgen32@gmail.com>
2019-04-06 13:55:13 +0600

    Moved view todo list to function

--- PyTODO.py
+++ PyTODO.py
@@ -3,8 +3,13 @@ from json import loads
 todo_list = open('todo_list.json').read()
 todo_list = loads(todo_list)
 
-while True:
+
+def view_todo_list():
     print("Список задач: \n")
     for i, c in enumerate(todo_list):
         print('[', i, ']', todo_list[i]['task'], '|', todo_list[i]['status'])
+
+
+while True:
+    view_todo_list()
     input()
